e8a0f5b8b5ba0589de8021f37285d4fec955938f
Soumya Ranjan Mohanty <soumyacool2012@gmail.com>
2019-03-27 04:09:36 +0530

    Connect with remote MySQL server

--- routes/api/channels.js
+++ routes/api/channels.js
@@ -8,23 +8,25 @@ module.exports = app => {
     res.send("Channels Working properly");
   });
 
-  app.post("/channels", async (req, res) => {
-    await Channel.find({}, async (err, channels) => {
-      await channels.forEach(
-        channel => (channel.rank = channel.rank.replace(",", "").slice(0, -2))
-      );
-      await channels.sort(function(a, b) {
-        if (Number(a.rank) < Number(b.rank)) {
-          return -1;
-        }
-        if (Number(a.rank) > Number(b.rank)) {
-          return 1;
-        }
-        return 0;
-      });
-      res.json({
-        channels
-      });
-    });
-  });
+
+
+  // app.post("/channels", async (req, res) => {
+  //   await Channel.find({}, async (err, channels) => {
+  //     await channels.forEach(
+  //       channel => (channel.rank = channel.rank.replace(",", "").slice(0, -2))
+  //     );
+  //     await channels.sort(function(a, b) {
+  //       if (Number(a.rank) < Number(b.rank)) {
+  //         return -1;
+  //       }
+  //       if (Number(a.rank) > Number(b.rank)) {
+  //         return 1;
+  //       }
+  //       return 0;
+  //     });
+  //     res.json({
+  //       channels
+  //     });
+  //   });
+  // });
 };
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const path = require("path");
+const mysql = require("mysql");
 require('dotenv').config()
 
 const app = express();
@@ -10,16 +11,82 @@ app.use(bodyParser.json());
 
 const channels = require("./routes/api/channels");
 
-require("./models/Channel.js");
+// require("./models/Channel.js");
 
 // MongoDB configuration
-const db = process.env.MONGO_URI;
+// const db = process.env.MONGO_URI;
 
 // Use mongoose to connect to mongoDB
-mongoose
-  .connect(db)
-  .then(() => console.log("MongoDB connected successfully!"))
-  .catch(err => console.log(err));
+// mongoose
+//   .connect(db)
+//   .then(() => console.log("MongoDB connected successfully!"))
+//   .catch(err => console.log(err));
+
+// MySQL connection
+var db_config = {
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB
+};
+
+
+var connection;
+
+function handleDisconnect() {
+    console.log('1. connecting to db:');
+    connection = mysql.createConnection(db_config); // Recreate the connection, since
+													// the old one cannot be reused.
+
+    connection.connect(function(err) {              	// The server is either down
+        if (err) {                                     // or restarting (takes a while sometimes).
+            console.log('2. error when connecting to db:', err);
+            setTimeout(handleDisconnect, 1000); // We introduce a delay before attempting to reconnect,
+        }                                     	// to avoid a hot loop, and to allow our node script to
+    });                                     	// process asynchronous requests in the meantime.
+    											// If you're also serving http, display a 503 error.
+    connection.on('error', function(err) {
+        console.log('3. db error', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') { 	// Connection to the MySQL server is usually
+            handleDisconnect();                      	// lost due to either server restart, or a
+        } else {                                      	// connnection idle timeout (the wait_timeout
+            throw err;                                  // server variable configures this)
+        }
+    });
+}
+app.get("/channels/work", (req, res) => {
+      connection.query(`create table if not exists test(
+                          data int primary key
+                      );`, function(err, rows, fields) {
+        if (err) {
+            console.log('error: ', err);
+            throw err;
+        }
+        res.send(['Hello World!!!! HOLA MUNDO!!!!', rows]);
+    });
+  });
+app.get("/channels/work1", (req, res) => {
+      connection.query(`INSERT INTO test(data)
+VALUES (13),(45),(23),(41),(34),(54)`, function(err, rows, fields) {
+        if (err) {
+            console.log('error: ', err);
+            throw err;
+        }
+        res.send(['Hello World!!!! HOLA MUNDO!!!!', rows]);
+    });
+  });
+
+app.get("/channels/work2", (req, res) => {
+      connection.query(`select * from test;`, function(err, rows, fields) {
+        if (err) {
+            console.log('error: ', err);
+            throw err;
+        }
+        res.send(['Hello World!!!! HOLA MUNDO!!!!', rows]);
+    });
+  });
+
+handleDisconnect();
 
 require("./routes/api/channels.js")(app);
 
